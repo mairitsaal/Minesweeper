239c3815c988d2c6b2a087ec044df6d79f16b233
Mairit Saal <mairit.saal@khk.ee>
2020-12-19 17:07:07 +0200

    Deploy docker15

--- techworld-js-docker-demo-app/app/server.js
+++ techworld-js-docker-demo-app/app/server.js
@@ -71,6 +71,6 @@ app.get('/get-profile', function (req, res) {
     });
 });
 
-app.listen(3009, function () {
-    console.log("app listening on port 3009!");
+app.listen(3000, function () {
+    console.log("app listening on port 3000!");
 });
